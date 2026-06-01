044a881bb4c9c42f4f21020f7ad076153af0fccd
Dikshitha A <dikshitha.anand7@gmail.com>
2026-06-01 22:30:26 +0530

    Added Java variables and data types example

---
+++ Basics/Variables.java
@@ -0,0 +1,28 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // Integer variable
+        int age = 20;
+
+        // Double variable
+        double height = 5.6;
+
+        // Character variable
+        char grade = 'A';
+
+        // Boolean variable
+        boolean isStudent = true;
+
+        // String variable
+        String name = "Dikshitha";
+
+        // Printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student Status: " + isStudent);
+    }
+}
+
