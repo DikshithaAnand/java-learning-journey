a682e19e601f2f260f8fee0df4259b175bed91d8
Dikshitha A <dikshitha.anand7@gmail.com>
2026-06-09 17:35:34 +0530

    Add TwoDimensionalArray class to demonstrate 2D arrays

---
+++ Basics/TwoDimensionalArray.java
@@ -0,0 +1,24 @@
+public class TwoDimensionalArray {
+
+    public static void main(String[] args) {
+
+        // Declaring and initializing 2D array
+        int[][] numbers = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Printing 2D array
+        System.out.println("2D Array Elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
